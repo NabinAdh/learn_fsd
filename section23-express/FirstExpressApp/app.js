814f8379818cafbe6b14055cdb392b16b07a9de0
Nabin Adhikari <mr.nabinadhikari@gmail.com>
2017-09-10 00:26:57 +1000

    Before starting Express Assignment

--- section23-express/FirstExpressApp/app.js
+++ section23-express/FirstExpressApp/app.js
@@ -13,10 +13,23 @@ app.get("/", function(req, res){
   res.send("Hi there");
 });
 
+// Listen on some pattern
+app.get("/post/:moreContent", function(req, res){
+  var content = req.params.moreContent;
+  res.send("Welcome to " + content);
+});
+
+
 // Listen on bye
 app.get("/bye", function(req, res){
   console.log(getFakeName() + " made request on /bye");
   res.send("Goodbye Mr");
+});
+
+// Listen on all other request, should be at last
+app.get("*", function(req, res){
+  console.log(getFakeName() + " made request on other page");
+  res.send("You are star");
 });
 
 // Start the server
